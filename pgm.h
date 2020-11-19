872126b2fe3dcb2b01bd53420111e762c0537258
gabirat <gabirat@onet.pl>
2020-11-19 13:39:29 +0100

    added pgm structure

---
+++ pgm.h
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#define NAME_LENGTH 128
+
+typedef struct {
+    char magic[2];
+    char name[NAME_LENGTH];
+    int16_t depth;
+    int32_t w;
+    int32_t h;
+} pgm_hdr;
+
+typedef struct {
+    pgm_hdr header;
+    int16_t* data;
+} pgm;
+
+int load_pgm(char* path, pgm image);
