3977d6bd5d16842ccbf71875de80dfac59350c8a
gabirat <gabirat@onet.pl>
2020-11-29 20:06:50 +0100

    New errors and print to stderr

---
+++ errors.c
@@ -0,0 +1,15 @@
+#include "errors.h"
+
+const char* SIGNALS_ERROR_CODES[] = {
+  "Not a valid PGM file (magic mismatch)",
+  "Can't allocate memory to load the file",
+  "Error reading file magic",
+  "Can't open image file",
+  "File version not implemented",
+  "Wrong file signature",
+  "Unknown error after file signature check"
+};
+
+void print_error_message(int error_code) {
+  fprintf(stderr, "Error code: %d\nMessage: %s\n", error_code, SIGNALS_ERROR_CODES[error_code - 1]);
+}
