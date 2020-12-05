02c30cda134a0be29f2d2016ea7cff773479bdb0
gabirat <gabirat@onet.pl>
2020-12-05 01:51:44 +0100

    tied everything up

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
+#include <time.h>
 #include "pgm.h"
+#include "errors.h"
+#include "menu.h"
 
-int main(void) {
-    return 0;
+int main() {
+  srand(time(NULL));
+  error_status err = init_error_status();
+  ctx* context;
+  err = init_ctx(&context);
+  if (err.err_t != NO_ERROR) {
+    print_error_message(err.err_no);
+    return err.err_no;
+  }
+  err = main_menu_loop(context);
+  if (err.err_t != NO_ERROR) {
+    print_error_message(err.err_no);
+    return err.err_no;
+  }
+  free_ctx(context);
+  return 0;
 }
